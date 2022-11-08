a0e97169be3740dc5457e33b9e7e3fa60b9162d0
Gianmarco Folchi <gmfolc@gmail.com>
2022-11-08 16:57:18 -0500

    edited login page to handle jwt and save it to device memory. Additionally, implemented the API call for register page

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,31 +1,46 @@
-import {
-	StyleSheet,
-	useWindowDimensions,
-	View,
-	Image,
-	Text,
-	ScrollView,
-	SafeAreaView,
-	Alert,
-	Button,
-} from "react-native";
-import React, { useState } from "react";
+import { StyleSheet, View, Image, Text, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
 import ProfilePicture from "../assets/images/profilePic.jpg";
 import CustomInput from "../components/CustomInput";
 import CustomButton from "../components/CustomButton";
-import EditProfileButton from "../components/EditProfileButton"
+import EditProfileButton from "../components/EditProfileButton";
 import { useNavigation } from "@react-navigation/native";
-import Settings from "react-native-vector-icons/Feather"
+import Settings from "react-native-vector-icons/Feather";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function ProfileScreen() {
+	const [userValues, setUserValues] = useState(null);
+	// return of api
+	// {"Error": null, "FirstName": "Test", "Following": ["6344e4ea7c568d2a25ed0f6f"], "Id": "634b740db076952180cb8e5a", "LastName": "User", "School": "", "Work": ""}
+	useEffect(() => {
+		const getProfileData = async () => {
+			const JSONuserInfo = await AsyncStorage.getItem("UserInfo");
+			const userInfo =
+				JSONuserInfo != null ? JSON.parse(JSONuserInfo) : null;
+			res = await fetch("http://localhost:8001/api/retrieveprofile", {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					_id: userInfo.userId,
+				}),
+			});
+			res = await res.json();
+			setUserValues(res);
+		};
+		getProfileData().catch(console.error);
+	}, []);
 
+	const onSettingPressed = () => {
+		console.log("settings pressed");
+	};
+	const onEditProfilePressed = () => {
+		console.log("profile edit button pressed");
+	};
 
-
-
-
-	
-
-    return(
+	return (
 		<SafeAreaView
 			contentContainerStyle={{
 				flexGrow: 1,
@@ -34,52 +49,62 @@ export default function ProfileScreen() {
 			}}
 			style={{ paddingBottom: 20 }}
 		>
+			{userValues && (
+				<View>
+					<View style={[styles.profileContainer]}>
+						<Image
+							source={ProfilePicture}
+							style={[styles.profile]}
+							resizeMode="contain"
+						/>
+					</View>
 
-			<View style={[styles.profileContainer]}>
-				<Image 
-					source={ProfilePicture} 
-					style={[styles.profile]}
-					resizeMode="contain"/>
-			</View>
+					<Settings
+						onPress={onSettingPressed}
+						name="settings"
+						size={30}
+						color={"#000"}
+						style={[styles.settingsIcon]}
+					/>
 
-			<Settings onPress={console.log("settings pressed")} name="settings" size={30} color={"#000"} style={[styles.settingsIcon]}/>
+					<View styles={styles.editProfileButton}>
+						<EditProfileButton
+							text={"Edit Profile"}
+							onPress={onEditProfilePressed}
+						/>
+					</View>
 
-			<View styles={styles.editProfileButton}>
-				<EditProfileButton
-					text={"Edit Profile"}
-					onPress={console.log("profile edit button pressed")}
-				/>
-			</View>
+					<View style={styles.nameContainer}>
+						<Text style={styles.name}>
+							{userValues.FirstName + " " + userValues.LastName}
+						</Text>
+					</View>
 
-			<View style={styles.nameContainer}>
-				<Text style={styles.name}>Obi Wan Kenobi</Text>
-			</View>
+					<View style={styles.userNameContainer}>
+						<Text>@vaderSux</Text>
+					</View>
 
-			<View style={styles.userNameContainer}>
-				<Text>@vaderSux</Text>
-			</View>
+					<View style={styles.followersContainer}>
+						<Text>25 Followers</Text>
+					</View>
 
-			<View style={styles.followersContainer}>
-				<Text>25 Followers</Text>
-			</View>
+					<View style={styles.followingContainer}>
+						<Text>122 Following</Text>
+					</View>
 
-			<View style={styles.followingContainer}>
-				<Text>122 Following</Text>
-			</View>
+					<View style={styles.textLineView}>
+						<View style={styles.line} />
+						<View style={styles.line} />
+					</View>
 
-			<View style={styles.textLineView}>
-				<View style={styles.line} />
-				<View style={styles.line} />
-			</View>
-
-			<View style={styles.messagesContainer}>
-				<Text>MESSAGES HERE</Text>
-			</View>
-
+					<View style={styles.messagesContainer}>
+						<Text>MESSAGES HERE</Text>
+					</View>
+				</View>
+			)}
 		</SafeAreaView>
 	);
 }
-
 
 const styles = StyleSheet.create({
 	root: {
@@ -93,48 +118,48 @@ const styles = StyleSheet.create({
 		height: 150,
 		width: 150,
 		borderRadius: 75,
-	  },
-	  profile: {
+	},
+	profile: {
 		height: 150,
 		width: 150,
 		borderRadius: 75,
-	  },
-	  nameContainer: {
+	},
+	nameContainer: {
 		left: 200,
-		bottom: 165
-	  },
-	  name: {
+		bottom: 165,
+	},
+	name: {
 		fontSize: 25,
 		fontWeight: "bold",
-	  },
-	  userNameContainer: {
+	},
+	userNameContainer: {
 		left: 200,
 		bottom: 160,
-	  },
-	  settingsIcon: {
+	},
+	settingsIcon: {
 		marginLeft: 375,
-		bottom: 150
-	  },
-	  followersContainer: {
+		bottom: 150,
+	},
+	followersContainer: {
 		marginLeft: 200,
-		bottom: 145
-	  },
-	  followingContainer: {
+		bottom: 145,
+	},
+	followingContainer: {
 		marginLeft: 310,
-		bottom: 162
-	  },
-	  textLineView: {
+		bottom: 162,
+	},
+	textLineView: {
 		justifyContent: "center",
 		alignItems: "center",
 		flexDirection: "row",
-		bottom: 130
+		bottom: 130,
 	},
 	line: {
 		flex: 1,
 		borderBottomColor: "black",
-		borderBottomWidth: 2
+		borderBottomWidth: 2,
 	},
 	messagesContainer: {
-		alignItems:"center",
-	}
+		alignItems: "center",
+	},
 });
